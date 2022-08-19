c0acdf11f618cf8508e64f08fc0bfe26e5d17d38
Mike <miketandy@protonmail.com>
2022-08-19 12:06:48 +0200

    mt: upd - convert to typescript

---
+++ src/types/data/ContentI.ts
@@ -0,0 +1,18 @@
+interface ContentI {
+  homepage: {
+    title1: string;
+    title1fa: string;
+    title2: string;
+    title2fa: string;
+    title3: string;
+    title3fa: string;
+    text1: string;
+    text1fa: string;
+    text2: string;
+    text2fa: string;
+    text3: string;
+    text3fa: string;
+  };
+}
+
+export default ContentI;
---
+++ src/types/data/TipI.ts
@@ -0,0 +1,11 @@
+interface TipI {
+  id: string;
+  title: string;
+  titlefa: string;
+  description: string;
+  descriptionfa: string;
+  banner: string;
+  img: string;
+}
+
+export default TipI;
